974b48bad72c32c00fcc9da02bc71d93f65d018b
jzeballos <jizeball@gmail.com>
2017-01-14 10:04:14 -0700

    problem3

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -32,7 +32,9 @@ function getName() {
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+function welcome()
+  var name1 = getName;
+  return "Welcome, " + name1; 
 
 
 //////////////////PROBLEM 4////////////////////
